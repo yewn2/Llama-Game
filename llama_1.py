6d4af833fbe958b43cc293dce4b17e501e85b667
yewn2 <yewn2@middleton.school.nz>
2025-05-04 16:56:04 +1200

    initializing game display

--- llama_1.py
+++ llama_1.py
@@ -14,7 +14,6 @@ pygame.display.set_caption("Llama game - by Nathan Yew")
 
 # Tuples containing colours to be used in game
 grey = (166, 166, 166)
-cream_white = (252, 245, 210)
 
 # Fonts to be used in game
 score_font_path = os.path.join("assets", "Press_Start_2P", "PressStart2P-Regular.ttf")
@@ -29,7 +28,6 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        screen.fill(cream_white)
         pygame.display.update()
 
 
